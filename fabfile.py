880c52c8c755601718b60e71f6e0db074968f52f
Pierre Bourdon <delroth@gmail.com>
2014-03-03 02:50:01 +0100

    Ajust fabfile for new buildbot deployment instructions.

--- fabfile.py
+++ fabfile.py
@@ -11,7 +11,8 @@ def lcd_project(name):
     return lcd(os.path.join(os.path.dirname(env.real_fabfile), name))
 
 def buildbot():
-    with cd("/home/buildbot/master"), lcd_project('buildbot'), \
-         settings(sudo_user='buildbot'):
-        put('master.cfg', 'master.cfg')
+    with cd("/home/buildbot/sadm"), settings(sudo_user='buildbot'):
+        sudo("git fetch origin master")
+        sudo("git reset --hard origin/master")
+    with cd("/home/buildbot/master"), settings(sudo_user='buildbot'):
         sudo("buildbot restart")

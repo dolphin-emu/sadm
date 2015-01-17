58bdcfb36204270f03ab4ddd3ac6752db53fba0f
Pierre Bourdon <delroth@gmail.com>
2015-01-17 18:42:04 +0100

    ec2: Add VPN configuration for the client, make buildslave initialization wait for network to be ready

--- ec2/ubuntu-nv/startup.sh
+++ ec2/ubuntu-nv/startup.sh
@@ -6,4 +6,21 @@ cd $(dirname $0)
 git fetch origin master
 git reset --hard origin/master
 
+openvpn --config openvpn.conf --daemon
+i=0
+while ! ip l | grep tap0; do
+  sleep 1
+  i=$((i+1))
+  if [ "$i" -gt 60 ]; then
+    halt
+  fi
+done
+ip l set tap0 mtu 1400
+
+for host in fifoci buildbot dl; do
+  echo "192.168.150.100 $host.dolphin-emu.org" >> /etc/hosts
+done
+
 python ../killswitch.py killswitch.yml &
+
+su - ubuntu -c "cd /home/ubuntu/buildslave && buildslave start"

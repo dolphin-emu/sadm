b3e464ddfd41b63d66e7b27ed26915038d43408a
Pierre Bourdon <delroth@gmail.com>
2015-07-31 01:59:25 +0200

    indexer: enable killswitch

--- ec2/ubuntu-indexer/startup.sh
+++ ec2/ubuntu-indexer/startup.sh
@@ -7,7 +7,7 @@ git fetch origin master
 git reset --hard origin/master
 
 # TODO(delroth): Enable once more testing has been done.
-# python ../killswitch.py killswitch.yml &
+python ../killswitch.py killswitch.yml &
 
 su - ubuntu -c "kythe-install.sh"
 su - ubuntu -c "cd /home/ubuntu/buildslave && buildslave start"

d363c757b120c42a63d71d6c28ce687d1dc1c5fc
Pierre Bourdon <delroth@gmail.com>
2015-08-01 15:40:59 +0200

    ec2 indexer: fix permissions

--- ec2/ubuntu-indexer/startup.sh
+++ ec2/ubuntu-indexer/startup.sh
@@ -4,6 +4,9 @@ export PATH=/bin:/usr/bin:/sbin:/usr/sbin
 
 cd $(dirname $0)
 
+# Used for temporary compilation/indexing products.
+chown ubuntu /mnt
+
 python ../killswitch.py killswitch.yml &
 
 su - ubuntu -c "cd sadm && git fetch origin master && git reset --hard origin/master"

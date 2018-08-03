be4fd55bb6031e29ebfd016600ba6b8dd611ffe4
Pierre Bourdon <delroth@gmail.com>
2018-08-03 12:14:17 +0200

    sadm: add Play Store updater cron

---
+++ android-updater/updater.py
@@ -0,0 +1,91 @@
+"""Updates the Play Store listing for Dolphin to follow an update track."""
+
+import argparse
+import httplib2
+import io
+import requests
+import sys
+
+from apiclient import discovery
+from apiclient import http as googhttp
+from oauth2client import service_account
+
+_PUBLISHER_SCOPE = 'https://www.googleapis.com/auth/androidpublisher'
+_PLAYSTORE_TRACK = 'production'
+_UPDATE_URL_FMT = 'https://dolphin-emu.org/update/latest/%s/'
+_ARTIFACT_ANDROID_SYSTEM = 'Android'
+_APK_MIME = 'application/vnd.android.package-archive'
+
+
+def _get_playstore_service(key_file):
+    credentials = service_account.ServiceAccountCredentials.from_json_keyfile_name(
+        key_file, scopes=[_PUBLISHER_SCOPE])
+    http = httplib2.Http()
+    http = credentials.authorize(http)
+    return discovery.build('androidpublisher', 'v3', http=http)
+
+
+def _get_dolphin_update_info(track):
+    return requests.get(_UPDATE_URL_FMT % track).json()
+
+
+def _get_prod_playstore_version(play, package_name):
+    edit_id = play.edits().insert(
+        body={}, packageName=package_name).execute()['id']
+    tracks = play.edits().tracks().list(
+        editId=edit_id, packageName=package_name).execute()
+    for track in tracks['tracks']:
+        if track['track'] != _PLAYSTORE_TRACK:
+            continue
+        return track['releases'][0]['name']
+
+
+def _upload_new_playstore_apk(play, package_name, apk, info):
+    edit_id = play.edits().insert(
+        body={}, packageName=package_name).execute()['id']
+    apk_response = play.edits().apks().upload(
+        editId=edit_id,
+        packageName=package_name,
+        media_body=googhttp.MediaIoBaseUpload(apk,
+                                              mimetype=_APK_MIME)).execute()
+    track_response = play.edits().tracks().update(
+        editId=edit_id,
+        packageName=package_name,
+        track=_PLAYSTORE_TRACK,
+        body={
+            'releases': [{
+                'name': info['shortrev'],
+                'versionCodes': [str(apk_response['versionCode'])],
+                'status': 'completed'
+            }]
+        }).execute()
+    play.edits().commit(editId=edit_id, packageName=package_name).execute()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--package_name', type=str, required=True)
+    parser.add_argument('--update_track', type=str, required=True)
+    parser.add_argument('--service_key_file', type=str, required=True)
+    args = parser.parse_args()
+
+    play = _get_playstore_service(args.service_key_file)
+
+    latest_dolphin_info = _get_dolphin_update_info(args.update_track)
+    playstore_version = _get_prod_playstore_version(play, args.package_name)
+
+    if latest_dolphin_info['shortrev'] == playstore_version:
+        print('Latest dolphin version %s is already on Play.' %
+              latest_dolphin_info['shortrev'])
+        sys.exit(0)
+
+    for artifact in latest_dolphin_info['artifacts']:
+        if artifact['system'] == _ARTIFACT_ANDROID_SYSTEM:
+            break
+    else:
+        print('No Android artifact found. Exiting.')
+        sys.exit(1)
+
+    apk = io.BytesIO(requests.get(artifact['url']).content)
+    _upload_new_playstore_apk(play, args.package_name, apk,
+                              latest_dolphin_info)

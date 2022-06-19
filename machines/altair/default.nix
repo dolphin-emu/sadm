dd47390f916c1308c9a630b6da86235d796f23c8
Pierre Bourdon <delroth@gmail.com>
2022-06-19 10:58:34 +0200

    altair: run Dolphin's Windows builder

--- machines/altair/default.nix
+++ machines/altair/default.nix
@@ -1,10 +1,16 @@
-{ self, pkgs, ... }:
+{ self, pkgs, nixpkgs, ... }:
 
 {
-  imports = [ ./hardware.nix ];
+  imports = [
+    ./hypervisor.nix
+    ./hardware.nix
+  ];
+
   services.openssh.enable = true;
   services.openssh.permitRootLogin = "prohibit-password";
   users.users.root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII3tjB4KYDok3KlWxdBp/yEmqhhmybd+w0VO4xUwLKKV" ];
+
+  nix.nixPath = [ "nixpkgs=${nixpkgs}" ];
 
   # Network configuration.
   networking.firewall.allowPing = true;
---
+++ machines/altair/hypervisor.nix
@@ -0,0 +1,80 @@
+# Configuration relating to the hypervisor / container runtime and internal
+# network used for CI builder guests.
+
+{ pkgs, lib, ... }:
+
+{
+  virtualisation.libvirtd.enable = true;
+  # https://github.com/NixOS/nixpkgs/issues/73418
+  systemd.services.libvirtd-config.script = lib.mkAfter ''
+    rm /var/lib/libvirt/qemu/networks/autostart/default.xml
+  '';
+
+  networking.bridges.br-guests.interfaces = [];
+  networking.interfaces.br-guests = {
+    ipv4.addresses = [ { address = "172.16.42.254"; prefixLength = 24; } ];
+    ipv6.addresses = [ { address = "2a01:4f8:191:44a:766d::1"; prefixLength = 80; } ];
+  };
+
+  networking.nat = {
+    enable = true;
+    internalInterfaces = [ "br-guests" ];
+    externalInterface = "enp41s0";
+  };
+
+  boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = true;
+  boot.kernel.sysctl."net.ipv6.conf.default.forwarding" = true;
+
+  services.dnsmasq = {
+    enable = true;
+    resolveLocalQueries = false;
+    extraConfig = ''
+      port=0  # Disable DNS
+
+      interface=br-guests
+      bind-interfaces
+
+      domain=builders.dolphin-emu.org
+
+      dhcp-option=option:dns-server,8.8.8.8,8.8.4.4
+      dhcp-option=option6:dns-server,2001:4860:4860::8888,2001:4860:4860::8844
+
+      dhcp-range=172.16.42.0,static,12h
+      dhcp-range=2a01:4f8:191:44a:766d::,2a01:4f8:191:44a:766d::ffff,static,80,12h
+      dhcp-authoritative
+      enable-ra
+
+      dhcp-host=52:54:00:a1:e7:1c,172.16.42.10,win2022
+      dhcp-host=52:54:00:a1:e7:1c,id:*,[2a01:4f8:191:44a:766d::10],win2022
+    '';
+  };
+  networking.firewall.allowedUDPPorts = [ 67 547 ];
+
+  systemd.services.guest-win2022 = {
+    after = [ "libvirtd.service" ];
+    requires = [ "libvirtd.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = "yes";
+    };
+
+    script = ''
+      ${pkgs.libvirt}/bin/virsh define ${./win2022.xml}
+      ${pkgs.libvirt}/bin/virsh start win2022
+    '';
+
+    preStop = ''
+      ${pkgs.libvirt}/bin/virsh shutdown win2022
+      let "timeout = $(date +%s) + 20"
+      while [ "$(${pkgs.libvirt}/bin/virsh list --name | grep --count '^win2022$')" -gt 0 ]; do
+        if [ "$(date +%s)" -ge "$timeout" ]; then
+          ${pkgs.libvirt}/bin/virsh destroy win2022
+        else
+          sleep 0.5
+        fi
+      done
+    '';
+  };
+}

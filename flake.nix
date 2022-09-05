81e33a3a12d5565bc3227d5e2dedb92b71c5f79c
Pierre Bourdon <delroth@gmail.com>
2022-09-05 10:58:17 +0200

    flake: add analytics-ingest input

--- flake.nix
+++ flake.nix
@@ -4,14 +4,18 @@
   inputs.agenix.url = "github:ryantm/agenix";
   inputs.agenix.inputs.nixpkgs.follows = "nixpkgs";
 
+  inputs.analytics-ingest.url = "github:dolphin-emu/analytics-ingest";
+  inputs.analytics-ingest.inputs.nixpkgs.follows = "nixpkgs";
+
   inputs.netplay-index.url = "github:dolphin-emu/netplay-index";
   inputs.netplay-index.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, agenix, netplay-index }@attrs: {
+  outputs = { self, nixpkgs, agenix, analytics-ingest, netplay-index }@attrs: {
     colmena = {
       meta.nixpkgs = import nixpkgs {
         system = "x86_64-linux";
         overlays = [
+          analytics-ingest.overlay
           netplay-index.overlay
         ];
       };

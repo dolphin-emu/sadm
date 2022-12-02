53843c2519ceb89c363920cbc5f98b4ce514c203
Pierre Bourdon <delroth@gmail.com>
2022-12-02 12:05:10 +0100

    flake.nix: provide our own poetry2nix to imported flakes

--- flake.nix
+++ flake.nix
@@ -6,13 +6,18 @@
   inputs.agenix.url = "github:ryantm/agenix";
   inputs.agenix.inputs.nixpkgs.follows = "nixpkgs";
 
+  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
+  inputs.poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
+
   inputs.analytics-ingest.url = "github:dolphin-emu/analytics-ingest";
   inputs.analytics-ingest.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.analytics-ingest.inputs.poetry2nix.follows = "poetry2nix";
 
   inputs.netplay-index.url = "github:dolphin-emu/netplay-index";
   inputs.netplay-index.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.netplay-index.inputs.poetry2nix.follows = "poetry2nix";
 
-  outputs = { self, nixpkgs, flake-utils, agenix, analytics-ingest, netplay-index }@attrs: {
+  outputs = { self, nixpkgs, flake-utils, analytics-ingest, netplay-index, ... }@attrs: {
     colmena = {
       meta.nixpkgs = import nixpkgs {
         system = "x86_64-linux";

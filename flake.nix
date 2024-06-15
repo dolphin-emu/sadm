a13fdc633fb3739b0cb1a51e6d926364c135b6c0
OatmealDome <julian@oatmealdome.me>
2024-06-15 16:59:52 -0400

    flake: Update to NixOS 24.05

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
